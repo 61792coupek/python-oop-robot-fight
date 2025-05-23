5594ad10fcba7dddf396b7c2aa9f3643a229cada
61792coupek <61792coupek@student.sstebrno.cz>
2025-05-23 10:54:42 +0200

    Add files via upload

---
+++ robot.py
@@ -0,0 +1,7 @@
+class Robot:
+    def __init__(self, name, hp, energy, attack_power, cooldown):
+        self.name = name
+        self.hp = hp
+        self.energy = energy
+        self.attack_power = attack_power
+        self.cooldown = cooldown
